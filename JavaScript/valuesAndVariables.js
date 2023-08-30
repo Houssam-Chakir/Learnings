de970528a58d1806cbc553f83e5acbcf1cd137a5
Houssam Chakir <80906408+Houssam-Chakir@users.noreply.github.com>
2023-08-30 16:03:08 +0100

    created operators.js and furter advancement in the course

--- JavaScript/valuesAndVariables.js
+++ JavaScript/valuesAndVariables.js
@@ -2,3 +2,23 @@ let country = "Morocco";
 let continent = "Africa";
 let population = 37000000;
 let NATIONALITY = "Moroccain";
+
+//* changing variable value and dynamic typing feature
+population = 'A lot of people I would say';
+
+//* let, const, and var:
+
+//? -let for variables that can change later
+//? can be created without assigned value
+//? Block scoped
+let age = 23;
+//one year later
+age = 24;
+
+//? -const for variables that will and cant be changed
+//! needs an initial value.
+const birthYear = 1999;
+
+//? -var is the legacy way of creating vars before the introductio of let and const
+//? function scoped
+//! should never use var
