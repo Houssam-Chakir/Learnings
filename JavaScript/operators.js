eaf08b72af34a98019725cbda489f058e5ba8435
Houssam Chakir <80906408+Houssam-Chakir@users.noreply.github.com>
2023-08-30 17:19:25 +0100

    operators notes

---
+++ JavaScript/operators.js
@@ -0,0 +1,9 @@
+//* Arithmatic operators
+const firstName = 'Houssam';
+const lastName = 'Chakir';
+
+const fullName = firstName + ' ' + lastName;
+//should equal Houssam Chakir 
+
+//? typeof operator to get the type of a value
+typeof fullName; //string
