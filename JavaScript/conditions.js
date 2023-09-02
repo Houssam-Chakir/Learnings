a30eeab34659edd6997b0dfc1791d3a65f49d300
Houssam Chakir <80906408+Houssam-Chakir@users.noreply.github.com>
2023-09-02 14:13:32 +0100

    switch statements

--- JavaScript/conditions.js
+++ JavaScript/conditions.js
@@ -1,4 +1,4 @@
-//: if / else statements
+//: IF / ELSE STATEMENTS ///////////////
 const age = 21;
 
 if(age >= 18) {
@@ -9,3 +9,32 @@ if(age >= 18) {
 
 //- one line if statement
 if(age === 21) console.log("cool");
+
+//: SWITCH STATEMENTS /////////////////
+//* good for comparing 1 value for multiple conditions
+
+const day = "Monday";
+
+switch (day) {
+    case "Monday": // day === "Monday"
+        console.log("time to get back to work :)");
+        console.log("study after work");
+        break; //! dont forget the break statement
+    case "Tuesday":
+        console.log("weekly meeting");
+        break;
+    case "Wednesday": // if its empty like this then both-
+    case "Thursday": //! wednesday and thursday share the same block
+        console.log("shifts and studies, the usual.");
+        break;
+    case "Friday":
+        console.log("Al jamuaa");
+        break;
+    case "Saturday":
+    case "Sunday":
+        console.log("gonna be the same");
+        console.log("my OFFs are scaterred in the week");
+        break;
+    default:
+        console.log("bruh only earth days");
+}
