aba1c4dd1f409c84f2c40ca2eb756aef5ca05001
Houssam Chakir <80906408+Houssam-Chakir@users.noreply.github.com>
2023-09-20 12:46:47 +0100

    this keyword section

--- JavaScript/this.js
+++ JavaScript/this.js
@@ -30,6 +30,36 @@ const calcAgeArrow = () => {
   console.log(2023 - birthYear);
   console.log(this);
 };
-calcAgeArrow(1999); // this refres to Window object 
+calcAgeArrow(1999); // this refres to Window object
 //* since arrow doesnt get its onw this so it uses the this keywork of parent function or parent scope
 //*
+
+//TODO // WHEN TO USE OR AVOID REGULAR AND ARROW FUNCTIONS //////////////
+
+var firstName = "Matilda";
+
+const houssam2 = {
+  name: "Houssam",
+  year: 1999,
+  // using an arrow function
+  greet: () => {
+    console.log(`Hey ${this.name}`); //this = Window object in this case
+  },
+
+  greetExpression: function () {
+    console.log(`Hey ${this.name}`); //this = houssam2
+  },
+
+  greetMatilda: () => {
+    console.log(this.firstName);
+  },
+};
+
+houssam2.greet(); //: Output: Hey undifined
+//- because this in this case is taking from the global scope
+//- the object is not considered a code block so it is not the parent, its the window object in this case
+
+houssam2.greetExpression(); //: Output: Hey Houssam
+//- generates a this keyword - takes parent which is houssam2 object
+
+houssam2.greetMatilda(); //: Output: Matilda
