c2f01f2157909ca0165ba2b1df95e36dd80a2889
Houssam Chakir <80906408+Houssam-Chakir@users.noreply.github.com>
2023-09-25 14:38:03 +0100

    for of loop notes

---
+++ JavaScript/FlowControl/Loops/forOfLoop.js
@@ -0,0 +1,12 @@
+const smgs = ["MP5", "MP7", "P90"];
+
+for (const gun of smgs) console.log(gun); //prints each gun
+
+//a way to get the index aswell
+for (const gun of smgs.entries()) {
+  console.log(gun); // something like: [0, "MP5"]
+}
+// destructuring  since gun is an array now
+for (const [index, gun] of smgs.entries()) {
+    console.log(`${index + 1}: ${gun}. [🛒 add to cart]`);
+}
