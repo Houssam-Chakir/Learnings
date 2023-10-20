c061ef391b64ddb7596dcbcf17a012c89ac1baa9
Houssam Chakir <80906408+Houssam-Chakir@users.noreply.github.com>
2023-10-20 20:42:47 +0100

    finished react crash course though i couldnt work with the api since it returns a promise instead of an object with Search prop

--- ReactJS/crash-course/src/App.js
+++ ReactJS/crash-course/src/App.js
@@ -1,10 +1,53 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { useEffect } from "react";
+import { render } from 'react-dom';
+
+import "./App.css";
+import searchIcon from "./search.svg";
+
+import MovieCard from "./movieCard";
+
+// c4a79159
+const API_URL = "http://www.omdbapi.com?apikey=c4a79159";
 
 const App = () => {
-    return(
-        <h1>Hello world</h1>
-    );
+  const moviesSearch = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = response.json();
+    console.log(data);
+  };
+
+  const movie1 = {
+    Title: "Batman Begins",
+    Year: "2005",
+    imdbID: "tt0372784",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+  };
+
+  useEffect(() => {
+    moviesSearch("Batman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Moviefy</h1>
+      {/*search div --------------- */}
+      <div className="search">
+        <input
+          placeholder="search for a movie"
+          value="superman"
+          onChange={() => {}}
+        ></input>
+        <img src={searchIcon} alt="search icon" onClick={() => {}} />
+      </div>
+      {/*container div ------------- */}
+      <div className="container">
+        <MovieCard movie={movie1} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
