1f2f17d68903a7c720fafdebea5329b3192d653c
houssam chakir <thisischchannel@gmail.com>
2024-01-02 16:03:27 +0100

    other simple array methods

--- JavaScript/arraysMethods/simpleMethods.js
+++ JavaScript/arraysMethods/simpleMethods.js
@@ -15,3 +15,28 @@ console.log(arr.concat(arr2)); // ['a', 'b', 'c', 'd', 'e', 'e', 'f', 'g', 'h', 
 console.log([...arr, ...arr2]); // also gives similar result without mutating original array
 
 console.log(arr.join('-')) //a-b-c-d-e
+
+//filling an array
+
+const x = new Array(7); // creates an empty array of 7 elements
+ x.fill(1, 3, 5); // it will fill it with ones starting from index 3 to 5
+
+//Array.from()
+const y = Array.from({length: 7}, () => {})
+
+//example ///////////
+// Array-like object representing temperatures in Celsius
+const temperaturesCelsius = { length: 3, 0: 20, 1: 25, 2: 30 };
+
+// Function to convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+// Using Array.from with mapFunction and thisArg
+const temperaturesFahrenheit = Array.from(temperaturesCelsius, function (celsius) {
+  return celsiusToFahrenheit(celsius);
+}, this);
+
+console.log(temperaturesFahrenheit);
+// Outputs: [68, 77, 86]
