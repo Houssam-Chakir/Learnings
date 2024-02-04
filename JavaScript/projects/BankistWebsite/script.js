8b7ab054149eb55c7b22603713de4da1e7746bf0
houssam chakir <thisischchannel@gmail.com>
2024-02-04 13:49:31 +0100

    Event delegation

--- JavaScript/projects/BankistWebsite/script.js
+++ JavaScript/projects/BankistWebsite/script.js
@@ -33,6 +33,9 @@ document.addEventListener("keydown", function (e) {
 //smooth scrolling
 const scrollToBtn = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const section2 = document.querySelector("#section--2");
+const section3 = document.querySelector("#section--3");
+const navLinks = document.querySelectorAll(".nav__link");
 console.log(scrollToBtn, section1);
 
 scrollToBtn.addEventListener("click", function (e) {
@@ -43,27 +46,38 @@ scrollToBtn.addEventListener("click", function (e) {
   });
 });
 
+//scroll to section
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if(e.target.classList.contains("nav__link")) {
+    const section = document.querySelector(e.target.getAttribute("href"));
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+});
 
 //random RGB color generator
-const randomRGB = () => {
-  let r = Math.floor(Math.random() * 256);
-  let g = Math.floor(Math.random() * 256);
-  let b = Math.floor(Math.random() * 256);
-  return `rgb(${r},${g},${b})`;
-};
+// const randomRGB = () => {
+//   let r = Math.floor(Math.random() * 256);
+//   let g = Math.floor(Math.random() * 256);
+//   let b = Math.floor(Math.random() * 256);
+//   return `rgb(${r},${g},${b})`;
+// };
 
 //EVENT BUBBLING
-document.querySelector('.nav__link').addEventListener('click', function (e) {
-  e.preventDefault();
-  this.style.color = randomRGB();
-});
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   e.preventDefault();
+//   this.style.color = randomRGB();
+// });
 
-document.querySelector('.nav__links').addEventListener('click', function (e) {
-  e.preventDefault();
-  this.style.backgroundColor = randomRGB();
-});
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   e.preventDefault();
+//   this.style.backgroundColor = randomRGB();
+// });
 
-document.querySelector('.nav').addEventListener('click', function (e) {
-  e.preventDefault();
-  this.style.backgroundColor = randomRGB();
-});
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   e.preventDefault();
+//   this.style.backgroundColor = randomRGB();
+// });
