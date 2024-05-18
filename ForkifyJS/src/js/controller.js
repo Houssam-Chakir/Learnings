b4400e9fb0ba7382a570a0557688d1cfa2a6fed4
houssam chakir <thisischchannel@gmail.com>
2024-05-19 00:01:36 +0100

    first fetch request

--- ForkifyJS/src/js/controller.js
+++ ForkifyJS/src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -9,5 +9,36 @@ const timeout = function (s) {
 };
 
 // https://forkify-api.herokuapp.com/v2
+// https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886
 
 ///////////////////////////////////////
+const singleRecipe = async function () {
+  try {
+    const res = await fetch(
+      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+    );
+    const data = await res.json();
+    //error handling invalid id
+    if(!res.ok) throw new Error(`Could not get recipe: ${data.message}`)
+    //console.log("data: ", data);
+
+    //reformatting data keys
+    let {recipe} = data.data
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    }
+    console.log('recipe: ', recipe);
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+singleRecipe();
